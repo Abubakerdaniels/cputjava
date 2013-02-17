9472a88564e6fdafad0e1509a5a08b766d9eecbb
daniels.abubaker@gmail.com <daniels.abubaker@gmail.com@862c9c63-e2c4-0cb8-0285-71876956b16e>
2013-02-17 19:40:02 +0000

    Test for true and false as well as null

--- isPalidDrome/src/com/Abubaker/IsPalidDromeImp.java
+++ isPalidDrome/src/com/Abubaker/IsPalidDromeImp.java
@@ -10,8 +10,14 @@ package com.Abubaker;
  */
 public class IsPalidDromeImp implements IsPalidDrome 
 {
-    protected  String status=new String("");
+    protected  String status="";
     private  IsPalidDromeImp number;
+    private  float digits;
+    
+    public IsPalidDromeImp()
+    {
+        
+    }
     
      @Override
      public boolean isPalindrome(int  digit)
@@ -33,6 +39,9 @@ public class IsPalidDromeImp implements IsPalidDrome
         }
         return false;
     }
+     
+     
+     @Override
      public boolean isNotPalindrome(int  digit)
      {
         int palindrome = digit; // copied digit into variable
@@ -52,29 +61,32 @@ public class IsPalidDromeImp implements IsPalidDrome
         }
         return false;
     }
-     
-     public String returnPalidrome()
+   
+     @Override
+     public   IsPalidDromeImp  ObjectPalidreturn(int digit)
      {
-        int palindrome = 101; 
-        int digit=8;// copied digit into variable
-        int playBack = 0;
-
-        while (palindrome != 0) 
-        {
-            int leftOver = palindrome % 10;
-            playBack = playBack * 10 + leftOver;
-            palindrome = palindrome / 10;
-        }
-
-        // if original and playBack of digit is equal means
-        // digit is palindrome in Java
-        if (  playBack == digit) 
-        {
-            return ("true");
-        }
-        return null;
+             IsPalidDromeImp palid;
+           //  palid=null;
+             palid=new IsPalidDromeImp();
+             
+             palid.setPalid(digit);
+             return palid;
      }
-
+     
+     @Override
+     public void setPalid(int digit)
+     {
+       digits=digit;   
+     }
+     @Override
+     public float  getPalid()
+     {
+          return digits;
+     }
+     
+     
+     
+     
 }
     
 
